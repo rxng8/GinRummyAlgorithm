b70a6f0a02c598f212de2a9db1fdf43185c56fb7
Alex Nguyen <alexvn.edu@gmail.com>
2020-05-24 14:21:00 -0400

    more data structure and architecture

--- ginrummy/gin_rummy.py
+++ ginrummy/gin_rummy.py
@@ -137,6 +137,10 @@ class InfoSet:
 1. What determine our strategy to pick card? Either draw face down or draw face up?
     - Whether we see that the card in the discarded pile benefit us (Form meld)? How much benefit? Probabilities?
     - Whether it increase the deadwood point or not ?
+
++ FOR THE ALGORITHM TO BE SIMPLE, WE WILL DRAW FACE UP IF THAT CARD FORM MELD, OTHERWISE DRAW FACE DOWN.
++ FOR FUTURE WORK, WE WILL LOOK INTO THE PROBABILITY OF FORMING MELDS OF EACH CARD, AND THE FACE UP CARD ALSO.
+    AND ALSO CONCATENATE WITH THE PROBABILITY OF 
 '''
 class DrawInfoSet (InfoSet):
     def __init__(self):
@@ -146,6 +150,9 @@ class DrawInfoSet (InfoSet):
         self.strategy = np.zeros(self.n)
         self.strategy_sum = np.zeros(self.n)
         self.op_cfr_values: List[float] = np.zeros(self.n)
+
+    def save_model(self):
+        pass
 
 
 '''
@@ -162,8 +169,12 @@ class DiscardInfoSet (InfoSet):
         self.strategy_sum = np.zeros(self.n)
         self.op_cfr_values: List[float] = np.zeros(self.n)
 
+    def save_model(self):
+        pass
+
 '''
 Data Structure of a public state.
+If so, we can have 52C10 * (52!) public states
 Params:
     1. Cards: List[Card]: List of Cards in hands
     2. Discards: List[Card]. List of cards in the discarded pile.
@@ -189,7 +200,7 @@ class GinTrainer:
 
         # Using arbitrary initial strategy profile for player
 
-        # Using arbitrary opponent
+        # Using arbitrary opponent range
 
         # Initialize regret
 
@@ -201,15 +212,22 @@ class GinTrainer:
         pass
 
     '''
-    Evaluate one step of a current player's turn.
+    Given a public state, evaluate a current player's turn with a tree builder within a certain depth.
     Params:
-        1.
+        1. depth_limit: int. This is the depth limit of the tree.
         2.
         3.
     Return:
     '''
-    def evaluate_step(self):
+    def evaluate_step(self, depth_limit=2):
+        # Build a tree with specified depth from a public state.
+
+        # Run cfr algorithm within a certain depth:
+        #
+
+
         pass
+
 
 
     '''
@@ -237,14 +255,74 @@ class GinTrainer:
     Return: Node actual value (utility)
     '''
     def cfr(self) -> float:
-        return 0.0
+        # Get player?
+        opponent = 1 - active_player
+
+        # Return payoff if state is terminal
+        if self.isTerminal(S: PublicState):
+            payoff = self.get_payoff(history, cards, active_player)
+            return payoff
+        
+        # new information and convert to public state to put in information set?
+        information = str(cards[active_player]) + history
+
+        # Get information set base on the public state or create if non-existed
+        infoSet = self.get_info_set(information)
+        infoSet.infoSet = information
+
+        # Get each strategy from the information set
+        strategy = infoSet.get_strategy(reach_probability_p0 if active_player == 0 else reach_probability_p1)
+
+        # For each action, call cfr with newer public state
+        
+        counterfactual_values = np.zeros(N_ACTIONS)
+        node_actual_value = 0
+
+        for i in range(N_ACTIONS):
+
+            if len(history) == 3:
+                return KuhnTrainer.get_payoff(history, cards, active_player)
+
+            counterfactual_values[i] =\
+                - self.cfr(cards, history + action, reach_probability_p0 * strategy[i], reach_probability_p1, opponent) if active_player == 0 else \
+                - self.cfr(cards, history + action, reach_probability_p0, reach_probability_p1 * strategy[i], opponent)
+            node_actual_value += strategy[i] * counterfactual_values[i]
+
+        # For each action, compute and accumulate counterfactual regret. (Regret Matching)
+        for i in range(N_ACTIONS):
+            counterfactual_regret = counterfactual_values[i] - node_actual_value
+            infoSet.regret_sum[i] += (reach_probability_p0 if active_player == 0 else reach_probability_p1) * counterfactual_regret
+        
+        return node_actual_value
+
+    '''
+    Check if a state is a terminal state
+    params:
+        1.
+        2.
+    return:
+    '''
+    @staticmethod
+    def is_terminal():
+        pass
+
+    '''
+    Reward function for the specified tree.
+    params:
+        1.
+        2.
+    return: 
+    '''
+    @staticmethod
+    def get_payoff():
+        pass
 
     def train(self):
         node_value = 0.0
         for _ in range(self.n_iter):
             node_value = self.cfr()
         
-        pass
+        return node_value
 
 # %%
 
@@ -257,4 +335,13 @@ class Util:
         pass
 
     def get_best_meld(self):
+        pass
+
+
+# %%
+'''
+Main game environment, with data generation (State) process
+'''
+class GinGame:
+    def __init__(self):
         pass

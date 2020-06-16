2c5f56eca3d67d559d9cc75bdbb9f28dc40f236d
Alex Nguyen <alexvn.edu@gmail.com>
2020-06-16 12:29:03 -0400

    successfully trained 100 epochs and saved model

--- ginrummy/GinRummyEAAIorigin/GinRummyEAAI/HESimpleModel.java
+++ ginrummy/GinRummyEAAIorigin/GinRummyEAAI/HESimpleModel.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.function.Function;
 
 public class HESimpleModel {
@@ -44,6 +45,15 @@ public class HESimpleModel {
 	 * 		3. Write Predicting method
 	 * 		4. Test thoroughly
 	 */
+
+	/**
+	 * ArrayList<ArrayList<ArrayList<ArrayList<short[][]>>>>: list of all games
+	 * ArrayList<ArrayList<ArrayList<short[][]>>>: List of all players
+	 * ArrayList<ArrayList<short[][]>>: List of all rounds.
+	 * ArrayList<short[][]>: List of all turns.
+	 * short[][]: One turn consisting of one input and one output.
+	 */
+	ArrayList<ArrayList<ArrayList<ArrayList<short[][]>>>> gamePlays;
 	
 	/**
 	 * Input value for curent state. Probability matrix of 52
@@ -113,23 +123,79 @@ public class HESimpleModel {
 	 * @param filename (String): The path the saved model.
 	 */
 	@SuppressWarnings("unchecked")
-	public HESimpleModel (String filename) {
+	public HESimpleModel (String weights, String bias) {
 		try {
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(weights));
 			this.weights1 = (float[][]) in.readObject();
 			this.weights2 = (float[][]) in.readObject();
 			in.close();
 			
 			if (this.VERBOSE) {
-				System.out.println("Saved to file " + filename);
+				System.out.println("Read weights from file " + weights);
 			}
 			
 		} catch (FileNotFoundException e) {
-			System.out.println("File not found: " + filename);
+			System.out.println("File not found: " + weights);
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(bias));
+			this.bias1 = (float[]) in.readObject();
+			this.bias2 = (float[]) in.readObject();
+			in.close();
+			
+			if (this.VERBOSE) {
+				System.out.println("Read bias from file " + bias);
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: " + bias);
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Save model, in this case, save weights.
+	 * @param filename
+	 */
+	public void save(String weights, String bias) {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(weights));
+			out.writeObject(this.weights1);
+			out.writeObject(this.weights2);
+			out.close();
+			if (this.VERBOSE) {
+				System.out.println("Written weights to file " + weights);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: " + weights);
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(bias));
+			out.writeObject(this.bias1);
+			out.writeObject(this.bias2);
+			out.close();
+			
+			if (this.VERBOSE) {
+				System.out.println("Written bias to file " + bias);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: " + bias);
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -152,23 +218,112 @@ public class HESimpleModel {
 	 * @param lr (float): learning rate.
 	 * @param n_iter (int): Number of epochs.
 	 */
-	public void __init__(float[][] X, float[][] Y, int seed, float lr, int n_iter) {
+	public void __init__(float[][] X, float[][] Y, int seed, float lr, int n_iter, boolean new_model) {
 		this.X = X;
 		this.Y = Y;
 		assert X.length == Y.length : "The number of training items from input and output must match!";
-		this.weights1 = new float[X[0].length][8];
-		this.bias1 = new float[this.weights1[0].length];
-		this.weights2 = new float[weights1[0].length][Y[0].length];
-		this.bias2 = new float[this.weights2[0].length];
+		if (new_model) {
+			this.weights1 = new float[X[0].length][8];
+			this.bias1 = new float[this.weights1[0].length];
+			this.weights2 = new float[weights1[0].length][Y[0].length];
+			this.bias2 = new float[this.weights2[0].length];
+		}
 		this.seed = seed;
 		this.lr = lr;
 		this.n_iter = n_iter;
 		
 		if (this.VERBOSE) {
-			System.out.println("Initialize Model...");
+			System.out.println("Initializing Model...");
+		}
+	}
+	
+	public void __init__(int seed, float lr, int n_iter, boolean new_model) {
+		preprocess_data();
+		if (new_model) {
+			this.weights1 = new float[X[0].length][8];
+			this.bias1 = new float[this.weights1[0].length];
+			this.weights2 = new float[weights1[0].length][Y[0].length];
+			this.bias2 = new float[this.weights2[0].length];
+		}
+		this.seed = seed;
+		this.lr = lr;
+		this.n_iter = n_iter;
+		
+		if (this.VERBOSE) {
+			System.out.println("Initializing Model...");
+		}
+	}
+	
+	/**
+	 * 
+	 * @param filename
+	 */
+	@SuppressWarnings("unchecked")
+	public void __import_data__(String filename) {
+		try {
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
+			this.gamePlays = (ArrayList<ArrayList<ArrayList<ArrayList<short[][]>>>>) in.readObject();
+			in.close();
+			if (this.VERBOSE) {
+				System.out.println("Imported data from file " + filename);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: " + filename);
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 *  Take the variable gameplays and preprocess to float[][] forms to put in X and Y
+	 * 	ArrayList<ArrayList<ArrayList<ArrayList<short[][]>>>> gamePlays
+	 */
+	public void preprocess_data() {
+		
+		assert this.gamePlays != null : "You have to import the data first using __import_data__ method";
+		
+		if (this.VERBOSE) {
+			System.out.println("Preprocessing data...");
+		}
+		
+		int data_size = this.gamePlays.size()
+				* this.gamePlays.get(0).size()
+				* this.gamePlays.get(0).get(0).size()
+				* this.gamePlays.get(0).get(0).get(0).size();
+		int data_feature = this.gamePlays.get(0).get(0).get(0).get(0)[0].length;
+		assert data_feature == this.gamePlays.get(0).get(0).get(0).get(0)[1].length : "Input and output does not match dimension";
+		
+		System.out.println("Data size: " + data_size);
+		
+		this.X = new float[data_size*10][data_feature];
+		this.Y = new float[data_size*10][data_feature];
+		
+		Iterator<ArrayList<ArrayList<ArrayList<short[][]>>>> it_games = this.gamePlays.iterator();
+		int i = 0;
+		while(it_games.hasNext()) {
+			Iterator<ArrayList<ArrayList<short[][]>>> it_players = it_games.next().iterator();
+			while (it_players.hasNext()) {
+				Iterator<ArrayList<short[][]>> it_rounds = it_players.next().iterator();
+				while (it_rounds.hasNext()) {
+					Iterator<short[][]> it_turns = it_rounds.next().iterator();
+					while (it_turns.hasNext()) {
+						short[][] turnData = it_turns.next();
+						assert turnData.length == 2 : "Wrong data form!";
+						for (int j = 0; j < turnData[0].length; j++) {
+							X[i][j] = (float) turnData[0][j];
+							Y[i][j] = (float) turnData[1][j];
+						}
+						i++;
+					}
+				}
+			}
 		}
 		
 	}
+	
 	
 	/**
 	 * Perform a sigmoid activation function
@@ -500,24 +655,6 @@ public class HESimpleModel {
 		return output;
 	}
 	
-	/**
-	 * Save model, in this case, save weights.
-	 * @param filename
-	 */
-	public void save(String filename) {
-		try {
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename));
-			out.writeObject(this.weights1);
-			out.writeObject(this.weights2);
-			out.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found: " + filename);
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
 	// Debugging method
 	
 	/**
@@ -570,6 +707,22 @@ public class HESimpleModel {
 		System.out.println();
 	}
 	
+	public static void print_mat1D_card(float[] mat, String name) {
+		System.out.println();
+		System.out.println(name + ": ");
+		int a = 0;
+		for (int i = 0; i < mat.length; i++) {
+			// Debugging
+			System.out.printf("%s: %.5f ",Card.getCard(i).toString(), mat[i]);
+			a++;
+			if(a == 13) {
+				a = 0;
+				System.out.println();
+			}
+		}
+		System.out.println();
+	}
+	
 	/**
 	 * 
 	 * @param v
@@ -588,31 +741,58 @@ public class HESimpleModel {
 //		float[][] x2 = {{2}, {3}, {0}};
 //		printMatrix(dot(x1, x2));
 		
-		float[][] X = {{-2, -1, 0, 0, 1},
-							{-2, 0, 1, 1, 0},
-							{-2, 1, 0, 1, 0},
-							{0, -2, -1, 0, 1}};
+//		float[][] X = {{-2, -1, 0, 0, 1},
+//							{-2, 0, 1, 1, 0},
+//							{-2, 1, 0, 1, 0},
+//							{0, -2, -1, 0, 1}};
+//		
+//		float[][] Y = {{0, 0, 0, 1, 1},
+//						{0, 1, 1, 1, 1},
+//						{0, 1, 1, 1, 1},
+//						{0, 0, 0, 1, 1}};
 		
-		float[][] Y = {{0, 0, 0, 1, 1},
-						{0, 1, 1, 1, 1},
-						{0, 1, 1, 1, 1},
-						{0, 0, 0, 1, 1}};
+//		HESimpleModel model = new HESimpleModel();
+//		model.__init__(X, Y, 10, 1e-2f, 300);
 		
-		HESimpleModel model = new HESimpleModel();
-		model.__init__(X, Y, 10, 1e-2f, 300);
 //		model.train();
 //		print_mat1D(HESimpleModel.compute_value(X[0], model.weights1, model.bias1, HESimpleModel::sigmoid));
-		model.weights1[1][2] = 12.3f;
+		
+//		model.weights1[1][2] = 12.3f;
+		
 //		model.evaluate_step();
-		model.set_verbose(true);
-		model.train();
+		
+//		model.set_verbose(true);
+//		model.train();
+		
 //		print_mat1D(HESimpleModel.compute_value(X[0], model.weights1, model.bias1, HESimpleModel::sigmoid));
 //		System.out.println(model.weights1[0].length);
-		System.out.print("Prediction for :");
-		HESimpleModel.print_mat1D(X[1]);
-		System.out.print("\n");
-		HESimpleModel.print_mat1D(model.predict(X[1]));
 		
+//		System.out.print("Prediction for :");
+//		HESimpleModel.print_mat1D(X[1]);
+//		System.out.print("\n");
+//		HESimpleModel.print_mat1D(model.predict(X[1]));
 		
+		/**
+		 * New model
+		 */
+//		HESimpleModel model = new HESimpleModel();
+//		model.__import_data__("play_data_SimplePlayer_small.dat");
+//		model.__init__(10, 10e-3f, 1, true);
+//		model.train();
+//		model.save("weights_100.dat", "bias_100.dat");
+		
+		/**
+		 * Load model
+		 */
+		HESimpleModel model = new HESimpleModel("weights_100.dat", "bias_100.dat");
+		model.__import_data__("play_data_SimplePlayer_small.dat");
+		model.__init__(10, 10e-3f, 1, false);
+		
+		/**
+		 * Predict hand.
+		 */
+		HESimpleModel.print_mat1D_card(model.X[2], "Input to be predicted");
+		HESimpleModel.print_mat1D_card(model.predict(model.X[2]), "Predicted");
+		HESimpleModel.print_mat1D_card(model.Y[2], "Actual Data");
 	}
 }

9eb2e62954c0f15b05cbf4ab6ed2611b05d69a17
Tom-from-6520 <cocplayer1alpha@gmail.com>
2020-08-09 14:49:54 -0400

    fixing player and its data collector

--- java/MavenProject/src/main/java/collector/EPlayerWeightDataCollector.java
+++ java/MavenProject/src/main/java/collector/EPlayerWeightDataCollector.java
@@ -118,6 +118,14 @@ public class EPlayerWeightDataCollector {
 					if(currentPlayer == 1 && isRecordingData) {
 						currentData[2] = drawFaceUp ? 0 : GinRummyUtil.getDeadwoodPoints(faceUpCard);
 						estimatingData.add(currentData);
+						
+						if(playVerbose) {
+							for(int i = 0; i < currentData.length; i++)
+								System.out.printf("%.4f ", currentData[i]);
+							System.out.println(); 
+						}
+						
+						currentData = null;
 					}
 					
 					if (playVerbose)
@@ -353,13 +361,14 @@ public class EPlayerWeightDataCollector {
 		
 		playVerbose = true;
 		collector.playWithEstimator();
+//		collector.playWithEstimator();
 		
-		playVerbose = false;
-		System.out.println("Playing games...");
-		int numGames = 1000;
-		
-		for(int i = 0; i < numGames; i++)
-			collector.playWithEstimator();
+//		playVerbose = false;
+//		System.out.println("Playing games...");
+//		int numGames = 100;
+//		
+//		for(int i = 0; i < numGames; i++)
+//			collector.playWithEstimator();
 		
 		collector.to_CSV("./dataset/est_sp_100_v1.csv", false);;
 		
